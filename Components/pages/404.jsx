41c80ef2a283192446b2cb819cda8a9c24c1f76b
DreamySuhail <140181169+DashingSuhail@users.noreply.github.com>
2023-12-11 09:59:17 +0530

    Add files via upload

--- Components/pages/404.jsx
+++ Components/pages/404.jsx
@@ -1,1 +1,10 @@
-
+function Page404() {
+	document.title = '404 - Page not found'
+	
+  return (
+    <div className="Page404">
+      <h2>404 - Page not found</h2>
+      <Link to="/">Return to home</Link>
+    </div>
+  )
+}
---
+++ Components/pages/home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+  document.title = 'Your Project - Home'
+  return (
+    <div className="Home">
+      Home
+    </div>
+  )
+}
